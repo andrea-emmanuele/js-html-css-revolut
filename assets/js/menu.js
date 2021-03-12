607093f6aad93057d62c0ef86c50d832516d9704
Andrea <git@andreaemmanuele.it>
2021-03-12 21:18:04 +0100

    Completed

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,30 +5,14 @@ $(function () {
 function dropdown() {
     let items = $(".right > .menu-item");
     let dropMenu;
-    console.log(items);
 
     items.mouseenter(function () {
         dropMenu = $(".drop-menu");
         dropMenu.eq(items.index(this)).addClass("active");
-        console.log(items.index(this));
     });
 
     items.mouseleave(function () {
         dropMenu = $(".drop-menu");
         dropMenu.eq(items.index(this)).removeClass("active");
-        console.log(items.index(this));
-    });
-
-    dropMenu = $(".drop-menu");
-
-    dropMenu.mouseenter(function () {
-        dropMenu.eq(items.index(this)).addClass("active");
-        console.log(items.index(this));
-    });
-
-    items.mouseleave(function () {
-        dropMenu = $(".drop-menu");
-        dropMenu.eq(items.index(this)).removeClass("active");
-        console.log(items.index(this));
     });
 }
